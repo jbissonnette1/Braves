f7722f54d06948ad79c3439b794d6c5ec7bf5ae1
jbissonnette <jbissonnette@bishopblanchet.org>
2014-05-15 08:37:27 -0700

    This is my change don't change it back

--- src/Brave.java
+++ src/Brave.java
@@ -2,6 +2,7 @@ public class Brave {
 
 	public static void main(String[] args) {
 		LinePrinter lp = new LinePrinter("Hello, world!");
-		lp.multiPrint(10);
+		lp.multiPrint(2);
+		System.out.print("Jane's change");
 	}
 }
